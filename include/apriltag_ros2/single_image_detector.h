3540da53e861d38aff2e8f5c8cc250ed7a705197
Vladislav Parkhachev <vladparhachev777@gmail.com>
2024-06-23 21:27:05 +0300

    Add declaration SingleImageDetector

--- include/apriltag_ros2/single_image_detector.h
+++ include/apriltag_ros2/single_image_detector.h
@@ -1,1 +1,23 @@
 #include "apriltag_ros2/common_functions.h"
+#include <apriltag_ros2/srv/analyze_single_image.hpp>
+
+#ifndef APRILTAG_ROS_SINGLE_IMAGE_DETECTOR_H
+#define APRILTAG_ROS_SINGLE_IMAGE_DETECTOR_H
+
+namespace apriltag_ros2
+{
+class SingleImageDetector
+{
+private:
+    TagDetector tag_detector_;
+    rclcpp::Service<apriltag_ros2::srv::AnalyzeSingleImage>::SharedPtr single_image_analysis_service_;
+    rclcpp::Publisher<apriltag_ros2::msg::AprilTagDetectionArray>::SharedPtr tag_detections_publisher_;
+private:
+    SingleImageDetector(std::shared_ptr<rclcpp::Node> node);
+
+    // The function which provides the single image analysis service
+    bool analyzeImage(apriltag_ros2::srv::AnalyzeSingleImage::Request& request, apriltag_ros2::srv::AnalyzeSingleImage::Response& response);
+};
+}
+
+#endif // APRILTAG_ROS_SINGLE_IMAGE_DETECTOR_H
