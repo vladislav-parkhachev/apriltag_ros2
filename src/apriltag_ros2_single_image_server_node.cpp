9e9951fa4f602908ebdffaae1f1b3e5ec16a0218
Vladislav Parkhachev <vladparhachev777@gmail.com>
2024-06-23 16:56:12 +0300

    Add server node

---
+++ src/apriltag_ros2_single_image_server_node.cpp
@@ -0,0 +1,17 @@
+#include "rclcpp/rclcpp.hpp"
+
+#include "apriltag_ros2/srv/analyze_single_image.hpp"
+
+
+int main(int argc, char **argv)
+{
+  rclcpp::init(argc, argv);
+
+  std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("apriltag_ros2_single_image_server");
+
+//   rclcpp::Service<apriltag_ros2::srv::AnalyzeSingleImage>::SharedPtr service = 
+//     node->create_service<apriltag_ros2::srv::AnalyzeSingleImage>("single_image_tag_detection", &add);
+
+  rclcpp::spin(node);
+  rclcpp::shutdown();
+}
