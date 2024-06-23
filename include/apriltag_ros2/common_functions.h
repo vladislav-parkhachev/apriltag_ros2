aa8e11a25f8951e127e64dddcc1b91c706ed0d2d
Vladislav Parkhachev <vladparhachev777@gmail.com>
2024-06-23 18:26:21 +0300

    Add StandaloneTagDescription

--- include/apriltag_ros2/common_functions.h
+++ include/apriltag_ros2/common_functions.h
@@ -8,4 +8,28 @@
 
 #include <apriltag.h>
 
+
+namespace apriltag_ros2
+{
+
+class StandaloneTagDescription
+{
+public:
+    StandaloneTagDescription() {};
+    StandaloneTagDescription(int id, double size, std::string &frame_name) : id_(id),size_(size), frame_name_(frame_name) {}
+
+    double size() { return size_; }
+    int id() { return id_; }
+    std::string& frame_name() { return frame_name_; }
+
+ private:
+    // Tag description
+    int id_;
+    double size_;
+    std::string frame_name_;
+};
+
+}
+
+
 #endif // APRILTAG_ROS_COMMON_FUNCTIONS_H
